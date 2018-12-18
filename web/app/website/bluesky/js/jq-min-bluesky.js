cd602ef97f36bd728e4e6116f16425401cce46b4
Rizal Farok <rizal@sidrablue.com.au>
2018-12-18 09:40:19 +0800

    visibility (lazy loading) implementation

--- web/app/website/bluesky/js/jq-min-bluesky.js
+++ web/app/website/bluesky/js/jq-min-bluesky.js
@@ -22,7 +22,9 @@ $(function(){
 
 	$('.special .image, .special.card .image, .special.cards .image').dimmer({ on:'hover' });
 	$('.dopop').click(function(){ $('.ui.modal.dapop').modal('show'); });
-	$('.shapefr').on('click',function(){ $('.shape').shape('flip right'); });
+	//$('.shapefr').on('click',function(){ $('.shape').shape('flip right'); });
+
+	$('img.ui.image, .ui.image img, .card .image img').visibility({ type: 'image', transition: 'fade in', duration: 2000 });
 
 	$('.ui.dropdown:not(#casefil)').dropdown();
 	$('#casefil.ui.dropdown').dropdown({
